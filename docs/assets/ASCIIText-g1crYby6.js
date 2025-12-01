723d4d082f18377f0d0e3e03f7a0075325083c09
Andre Boufama <andreboufama@dhcp-vl2041-54381.eduroam.cornell.edu>
2025-12-01 13:10:37 -0500

    Title overlapping cards -10px

--- docs/assets/ASCIIText-g1crYby6.js
+++ docs/assets/ASCIIText-g1crYby6.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-CEQKSMOR.js";/**
+import{r as vi,j as lo}from"./index-mYlOkFek.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
