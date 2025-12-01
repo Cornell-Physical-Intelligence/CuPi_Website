7a4271c1ccd5323ca14ef27f45d54f92e63ca8fb
Andre Boufama <andreboufama@dhcp-vl2041-54381.eduroam.cornell.edu>
2025-12-01 03:15:25 -0500

    Apply blur filter directly to menu when lightbox opens

--- docs/assets/ASCIIText-Dr4jMxFG.js
+++ docs/assets/ASCIIText-Dr4jMxFG.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-DdMl5Mii.js";/**
+import{r as vi,j as lo}from"./index-BBtj_2Wz.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
