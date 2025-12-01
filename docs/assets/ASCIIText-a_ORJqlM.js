3b798dfaa719632e01cfe25c701879f94b1b10c5
Andre Boufama <andreboufama@dhcp-vl2041-54381.eduroam.cornell.edu>
2025-12-01 03:13:41 -0500

    Menu gets blurred with backdrop instead of disappearing when lightbox opens

--- docs/assets/ASCIIText-a_ORJqlM.js
+++ docs/assets/ASCIIText-a_ORJqlM.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-BoxREuaD.js";/**
+import{r as vi,j as lo}from"./index-DdMl5Mii.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
