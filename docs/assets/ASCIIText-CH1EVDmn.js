cfde3411a65b23785d0ee4ad597571ee1e225b9a
Andre Boufama <andreboufama@dhcp-vl2041-21365.eduroam.cornell.edu>
2026-01-29 10:09:34 -0500

    Make about page team cards two-per-row on mobile

--- docs/assets/ASCIIText-CH1EVDmn.js
+++ docs/assets/ASCIIText-CH1EVDmn.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-cWvKH0AJ.js";/**
+import{r as vi,j as lo}from"./index-C7Cne8yO.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
