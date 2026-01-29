a9e033df8f85ba7ebf25f51610b2d5bff25d78b5
Andre Boufama <andreboufama@dhcp-vl2041-21365.eduroam.cornell.edu>
2026-01-29 10:56:16 -0500

    Hide hover tag label on mobile, keep dotted border only

--- docs/assets/ASCIIText-BIPtTF8E.js
+++ docs/assets/ASCIIText-BIPtTF8E.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-B0lI1jAP.js";/**
+import{r as vi,j as lo}from"./index-DOWvsqCu.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
