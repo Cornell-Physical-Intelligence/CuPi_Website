cf02b712e568d37f0de9efbf6fe9b02753fb4d47
Andre Boufama <andreboufama@dhcp-vl2041-21365.eduroam.cornell.edu>
2026-01-29 10:47:01 -0500

    Hide LegRender background image on mobile
    Remove the leg photo on small screens and reduce the excess
    padding/min-height that was reserving space for it.

--- docs/assets/ASCIIText-B7d-5rsn.js
+++ docs/assets/ASCIIText-B7d-5rsn.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-5GO1Hdy5.js";/**
+import{r as vi,j as lo}from"./index-U9CqMuB2.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
