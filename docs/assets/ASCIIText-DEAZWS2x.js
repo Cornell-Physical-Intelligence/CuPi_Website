599eba9a74ec7d1413aef7beadd62d1918ff1a3b
Andre Boufama <andreboufama@dhcp-vl2041-39843.eduroam.cornell.edu>
2026-02-03 19:00:51 -0500

    Delay project overlay fade on flip

--- docs/assets/ASCIIText-DEAZWS2x.js
+++ docs/assets/ASCIIText-DEAZWS2x.js
@@ -1,4 +1,4 @@
-import{r as vi,j as lo}from"./index-CO8W3hC8.js";/**
+import{r as vi,j as lo}from"./index-B07YSw0c.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
