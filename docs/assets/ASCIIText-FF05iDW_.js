66868a57c5bc5d771c68533e3db3ad3eaad03d10
Andre Boufama <andreboufama@dhcp-vl2041-39843.eduroam.cornell.edu>
2026-02-04 00:27:03 -0500

    Rebuild docs/ for GitHub Pages deployment

--- docs/assets/ASCIIText-FF05iDW_.js
+++ docs/assets/ASCIIText-FF05iDW_.js
@@ -1,4 +1,4 @@
-import{r as Rt,j as lo}from"./index-BLPqZAqy.js";/**
+import{r as Rt,j as lo}from"./index-BBIDP1fY.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
