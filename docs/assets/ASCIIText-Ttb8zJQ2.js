48bc506bf02ba89b412071eaa087c3b28f8e475e
Andre Boufama <ab3233@cornell.edu>
2026-03-16 17:34:28 -0400

    Adjust team card corner radius

--- docs/assets/ASCIIText-Ttb8zJQ2.js
+++ docs/assets/ASCIIText-Ttb8zJQ2.js
@@ -1,4 +1,4 @@
-import{r as Rt,j as lo}from"./index-DA4zHT6h.js";/**
+import{r as Rt,j as lo}from"./index-9oNOD_wP.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
